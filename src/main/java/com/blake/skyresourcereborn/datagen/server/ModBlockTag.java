a5eb893f1b16a21f3e0e7baf106c74aec78c52c6
DevDyna <andrearisorti2003@gmail.com>
2025-04-24 21:27:57 +0200

    tags

--- src/main/java/com/blake/skyresourcereborn/datagen/server/ModBlockTag.java
+++ src/main/java/com/blake/skyresourcereborn/datagen/server/ModBlockTag.java
@@ -3,20 +3,35 @@ package com.blake.skyresourcereborn.datagen.server;
 import java.util.concurrent.CompletableFuture;
 
 import com.blake.skyresourcereborn.SkyResourceReborn;
+import com.blake.skyresourcereborn.registry.*;
 
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+
 @SuppressWarnings("null")
-public class ModBlockTag extends BlockTagsProvider{
+public class ModBlockTag extends BlockTagsProvider {
 
     public ModBlockTag(PackOutput o, CompletableFuture<Provider> l, ExistingFileHelper f) {
-                super(o, l, SkyResourceReborn.MODID, f);
-        }
+        super(o, l, SkyResourceReborn.MODID, f);
+    }
 
     @Override
     protected void addTags(Provider p) {
+        tag(ModBlockTags.CORE).add(
+                ModBlocks.MULTIBLOCK_CORE.get());
 
+        tag(ModBlockTags.HATCHES).add(
+                ModBlocks.ENERGY_INPUT.get(),
+                ModBlocks.ITEM_INPUT.get(),
+                ModBlocks.ITEM_OUTPUT.get());
+
+        tag(ModBlockTags.INPUT_HATCHES).add(
+                ModBlocks.ENERGY_INPUT.get(),
+                ModBlocks.ITEM_INPUT.get());
+
+        tag(ModBlockTags.OUTPUT_HATCHES).add(
+                ModBlocks.ITEM_OUTPUT.get());
     }
 }
--- src/main/java/com/blake/skyresourcereborn/datagen/server/ModItemTag.java
+++ src/main/java/com/blake/skyresourcereborn/datagen/server/ModItemTag.java
@@ -2,10 +2,13 @@ package com.blake.skyresourcereborn.datagen.server;
 
 import java.util.concurrent.CompletableFuture;
 
+import com.blake.skyresourcereborn.registry.*;
+
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.world.level.block.Block;
+
 @SuppressWarnings("null")
 public class ModItemTag extends ItemTagsProvider {
 
@@ -16,7 +19,20 @@ public class ModItemTag extends ItemTagsProvider {
 
     @Override
     protected void addTags(Provider p) {
+        tag(ModItemTags.CORE).add(
+                ModBlocks.MULTIBLOCK_CORE.get().asItem());
 
+        tag(ModItemTags.HATCHES).add(
+                ModBlocks.ENERGY_INPUT.get().asItem(),
+                ModBlocks.ITEM_INPUT.get().asItem(),
+                ModBlocks.ITEM_OUTPUT.get().asItem());
+
+        tag(ModItemTags.INPUT_HATCHES).add(
+                ModBlocks.ENERGY_INPUT.get().asItem(),
+                ModBlocks.ITEM_INPUT.get().asItem());
+
+        tag(ModItemTags.OUTPUT_HATCHES).add(
+                ModBlocks.ITEM_OUTPUT.get().asItem());
     }
 
 }
