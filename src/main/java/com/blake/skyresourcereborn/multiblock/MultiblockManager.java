1375f1fa719f0c2b29dbb3af4fce58a55140d33d
Salvatore Liccardo <s.liccardo022@gmail.com>
2025-04-24 12:50:05 +0200

    accept multiple recipes in json

--- src/main/java/com/blake/skyresourcereborn/multiblock/MultiblockManager.java
+++ src/main/java/com/blake/skyresourcereborn/multiblock/MultiblockManager.java
@@ -14,7 +14,6 @@ import java.util.*;
 
 @Mod.EventBusSubscriber(modid = "skyresourcereborn")
 public class MultiblockManager {
-
     public static final Map<ResourceLocation, MultiblockStructure> STRUCTURES = new HashMap<>();
 
     @SubscribeEvent
@@ -23,43 +22,45 @@ public class MultiblockManager {
             @Override
             protected void apply(Map<ResourceLocation, JsonElement> jsonMap, ResourceManager resourceManager, ProfilerFiller profiler) {
                 STRUCTURES.clear();
-
                 for (var entry : jsonMap.entrySet()) {
                     ResourceLocation id = entry.getKey();
                     JsonObject json = entry.getValue().getAsJsonObject();
+
                     List<MultiblockStructure.BlockOffset> structure = new ArrayList<>();
                     JsonArray array = json.getAsJsonArray("structure");
-
                     for (JsonElement element : array) {
                         JsonObject obj = element.getAsJsonObject();
                         JsonArray offsetArr = obj.getAsJsonArray("offset");
-
                         BlockPos offset = new BlockPos(
                                 offsetArr.get(0).getAsInt(),
                                 offsetArr.get(1).getAsInt(),
                                 offsetArr.get(2).getAsInt()
                         );
-
                         ResourceLocation blockId = ResourceLocation.parse(obj.get("block").getAsString());
                         structure.add(new MultiblockStructure.BlockOffset(offset, blockId));
                     }
-                    JsonObject processing = json.getAsJsonObject("processing");
-                    String input = processing.get("input").getAsString();
-                    String output = processing.get("output").getAsString();
-                    int time = processing.get("time").getAsInt();
 
-                    MultiblockProcessing processingData = new MultiblockProcessing(input, output, time);
+                    List<MultiblockStructure.Recipe> recipes = new ArrayList<>();
+                    JsonArray processingArray = json.getAsJsonArray("processing");
+                    for (JsonElement element : processingArray) {
+                        JsonObject recipeJson = element.getAsJsonObject();
+                        recipes.add(new MultiblockStructure.Recipe(
+                                recipeJson.get("input").getAsString(),
+                                recipeJson.get("output").getAsString(),
+                                recipeJson.get("time").getAsInt()
+                        ));
+                    }
+
                     MultiblockStructure s = new MultiblockStructure(
                             id,
                             structure,
                             json.get("requires_energy").getAsBoolean(),
                             json.get("energy_per_tick").getAsInt(),
-                            processingData
+                            recipes
                     );
 
                     STRUCTURES.put(id, s);
                 }
-                System.out.println("✅ Caricate " + STRUCTURES.size() + " strutture multiblock");
             }
         });
     }
