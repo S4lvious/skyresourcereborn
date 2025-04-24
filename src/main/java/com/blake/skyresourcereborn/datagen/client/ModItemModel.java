3c75b25e3dea7b3f626af0505e778f6cc5039b8e
DevDyna <andrearisorti2003@gmail.com>
2025-04-24 22:53:50 +0200

    fixed item model datagen

--- src/main/java/com/blake/skyresourcereborn/datagen/client/ModItemModel.java
+++ src/main/java/com/blake/skyresourcereborn/datagen/client/ModItemModel.java
@@ -2,18 +2,14 @@ package com.blake.skyresourcereborn.datagen.client;
 
 import static com.blake.skyresourcereborn.SkyResourceReborn.MODID;
 
-import java.util.Set;
-
 import com.blake.skyresourcereborn.registry.ModBlocks;
-import com.blake.skyresourcereborn.utils.DataGenUtil;
 import com.blake.skyresourcereborn.utils.ResourceLocationUtil;
 
 import net.minecraft.data.PackOutput;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.registries.RegistryObject;
 
 public class ModItemModel extends ItemModelProvider {
 
@@ -26,19 +22,17 @@ public class ModItemModel extends ItemModelProvider {
     @Override
     protected void registerModels() {
 
-        //TODO to finish
-    //     DataGenUtil.itemBlockwithParent(ModBlocks.MULTIBLOCK_CORE, null, ResourceLocationUtil
-    //                     .getResource("block/" +
-    //                             ResourceLocationUtil.getPath(Blocks.REDSTONE_BLOCK), "minecraft"));
-    //     DataGenUtil.itemBlockwithParent(ModBlocks.ENERGY_INPUT, null, "");
-    //     DataGenUtil.itemBlockwithParent(ModBlocks.ITEM_INPUT, null, "");
-    //     DataGenUtil.itemBlockwithParent(ModBlocks.ITEM_OUTPUT, null, "");
-    //    DemoBlocks(, Blocks.REDSTONE_BLOCK);
-    //     DemoBlocks(ModBlocks.ENERGY_INPUT, Blocks.REDSTONE_LAMP);
-    //     DemoBlocks(ModBlocks.ITEM_INPUT, Blocks.LIME_WOOL);
-    //     DemoBlocks(ModBlocks.ITEM_OUTPUT, Blocks.LIGHT_BLUE_WOOL);
+        copy(ModBlocks.MULTIBLOCK_CORE.get(),Blocks.REDSTONE_BLOCK);
+        copy(ModBlocks.ENERGY_INPUT.get(), Blocks.REDSTONE_LAMP);
+        copy(ModBlocks.ITEM_INPUT.get(), Blocks.LIME_WOOL);
+        copy(ModBlocks.ITEM_OUTPUT.get(), Blocks.LIGHT_BLUE_WOOL);
 
 
+    }
+
+    private void copy(Block base,Block copy){
+        this.withExistingParent(ResourceLocationUtil.getPath(base),
+                 "minecraft:block/" + ResourceLocationUtil.getPath(copy));
     }
 
 }
